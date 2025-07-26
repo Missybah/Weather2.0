49dbc44f7a258401e90a5b57a06136cd2f4a2252
Misbah <Misbahsaleh0@gmail.com>
2025-07-26 11:26:47 +0100

    Fixed api error

--- weather.js
+++ weather.js
@@ -2,7 +2,6 @@ function refreshWeather(response) {
   // Example: update city and temperature
   let cityElement = document.querySelector("#weather-app-city");
   let temperatureElement = document.querySelector(".weather-app-temperature");
-  let temperature = response.data.temperature.current;
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
@@ -14,10 +13,7 @@ function refreshWeather(response) {
   console.log(response.data.condition.description);
 
   cityElement.innerHTML = response.data.city;
-  timeElement.innerHTML = `$date.getDay()} ${date.getHours()}:${date
-    .getMinutes()
-    .toString()
-    .padStart(2, "0")}`;
+  timeElement.innerHTML = formatDate(date);
   descriptionElement = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
@@ -28,6 +24,14 @@ function refreshWeather(response) {
 
 function handleSearchSubmit(event) {
   event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let city = searchInput.value;
+  let apiKey = "5c96e661b5c38754d1f19tae0off360c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
   let days = [
@@ -45,14 +49,6 @@ function handleSearchSubmit(event) {
     minutes = `0${minutes}`;
   }
   return `${day} ${hours}:${minutes}`;
-}
-let searchInput = document.querySelector("#search-form-input");
-let city = searchInput.value.trim();
-
-let apiKey = "5c96e661b5c38754d1f19tae0off360c";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-{
-  axios.get(apiUrl).then(refreshWeather);
 }
 
 let searchFormElement = document.querySelector("#search-form");
